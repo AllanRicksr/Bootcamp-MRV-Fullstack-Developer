24093945088683dcba64a702cf486bbc3b1a0137
AllanRicksr <allanrickrodrigues@hotmail.com>
2022-02-14 19:03:04 -0400

    JavaScript Aula(5)

---
+++ JavaScript/aula 2/script.js
@@ -0,0 +1,63 @@
+// //tipos primitivos
+
+// // boolean
+// var vouf = false
+// console.log(typeof(vouf));
+
+// //number
+
+// var numero = 1
+// console.log(typeof(numero));
+
+// //string
+// var nome = 'diana';
+// console.log(typeof(nome));
+
+
+// // como declarar
+// var variavel = 'diana';
+// variavel = 'mari';
+
+// let variavel2 = 'diana';
+// variavel2 = 'mari';
+// console.log(variavel2);
+
+// const constante = 'mria';
+// console.log(constante);
+
+// var escopoGlobal = 'global';
+// console.log(escopoGlobal);
+
+// function escopolocal() {
+//     let escopoLocalInterno = 'local';
+//     console.log(escopoLocalInterno)
+// }
+
+// // escopolocal();
+
+// //atribuição
+// var atriuicao = 'mari';
+
+// //comparação
+// var compracao = '0' == 0;
+// console.log(comparacao);
+
+// // comparação identica
+// var comparacaoidentica = '0' === 0;
+// console.log(comparacaoidentica);
+
+// // maior que
+// var maiorque = 5 > 2;
+// console.log(maiorque);
+
+// //menor que
+// var menorque = 5 < 2;
+// console.log(menorque);
+
+// //maior ou igual a
+// var maiorouigual = 5 => 2;
+// console.log(maiorouigual);
+
+// //menor ou igual a
+// var menorouigual = 5 <= 2;
+// console.log(menorouigual);
