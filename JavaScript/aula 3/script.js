b3d7a699007a435f9d5e18f0633f773c2437fba0
AllanRicksr <allanrickrodrigues@hotmail.com>
2022-02-15 17:17:45 -0400

    JavaScript Aula(6)

---
+++ JavaScript/aula 3/script.js
@@ -0,0 +1,51 @@
+// o que são vetores ou arrays
+
+// como declarar um array
+// let array = ['string', 1 , true];
+// console.log(array);
+
+//pode guardar vários tipos de dados
+// let array = ['string', 1, true, ['array 1'], ['array 2'], ['array 3'],]
+// console.log(array[3])
+
+// forEach
+// Array.array.forEach(function(item, index){console.log(item, index)});
+
+//push
+// array.push(1)
+// console.log(array)
+
+//pop
+// array.pop()
+// console.log(array);
+
+//shift
+// array.shift()
+// console.log(array);
+
+//unshift
+// array.unshift('novo item no inicio')
+// console.log(array);
+
+// indexof
+// console.log(array.indexOf(true));
+
+//splice
+// array.splice(0, 3);
+// console.log(array);
+
+// slice
+// let novoarray = array.slice(0, 3);
+// console.log(array);
+
+// let object = {string: 'string', number: 1, boolean: true, Array ['array'], obijetointerno: {objectinterno}};
+// console.log(object.obijetointerno);
+
+// var string = object.string;
+// console.log(string);
+
+// var arrayInterno = object.array;
+// console.log(arrayInterno);
+
+// var { string, boolean, objectinterno} = object;
+// console.log(string, boolean, objectinterno);
