47eb826f9e4ee98d261f16c21b45e8268a61a898
AllanRicksr <allanrickrodrigues@hotmail.com>
2022-02-17 11:04:01 -0400

    JavaScript Aula(9)

---
+++ JavaScript/aula 6/script.js
@@ -0,0 +1,23 @@
+// tipos de função
+
+// // declarativas
+// function funcao(){
+//     console.log('alt')
+// }
+
+// funcao();
+
+// // expressão de função
+// // com nomeação
+// var funcao = function(){
+//     console.log('Mensagem')
+// }
+
+// funcao();
+
+// arrow function
+// var funcao = () => {
+//     console.log('arrow function')
+// }
+
+// funcao();
